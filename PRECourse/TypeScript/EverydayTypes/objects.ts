e49690b9fa2f860d6d42655425c23cd87068c954
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-04-25 21:48:09 +0200

    Właściwości opcjonalne
    Gdy właściwość jest opcjonalna trzeba koniecznie sprawdzić czy jej
    wartość = undefined.

--- PRECourse/TypeScript/EverydayTypes/objects.ts
+++ PRECourse/TypeScript/EverydayTypes/objects.ts
@@ -5,3 +5,19 @@ function printCoord(pt: { x: number; y: number }) {
 }
 
 printCoord({ x: 3, y: 7 });
+
+function printName(obj: { first: string; last?: string }) {
+    // Error - might crash if 'obj.last' wasn't provided!
+
+    if (obj.last !== undefined) {
+        // OK
+        console.log(obj.last.toUpperCase());
+    }
+
+    // A safe alternative using modern JavaScript syntax:
+    console.log(obj.last?.toUpperCase());
+}
+
+// Both OK
+printName({ first: 'Bob' });
+printName({ first: 'Alice', last: 'Alisson' });
