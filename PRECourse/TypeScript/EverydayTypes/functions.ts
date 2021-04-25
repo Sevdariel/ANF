4b4100166ad8e824c0678c1526926c3088c9e541
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-04-25 21:29:13 +0200

    Typ parametru
    Wywołanie funkcji z złym typem argumentu spowoduje wyrzucenie błedu

---
+++ PRECourse/TypeScript/EverydayTypes/functions.ts
@@ -0,0 +1,8 @@
+// Parameter type annotation
+function greet(name: string) {
+    console.log('Hello, ' + name.toUpperCase() + '!!');
+}
+
+// Would be a runtime error if executed!
+// Runtime error: Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+// greet(42);
