a70418b1950f50098bb8303f8f3a3f437e6fbe3d
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-04-25 17:44:36 +0200

    Użycie typów argumentów
    person = string
    date = Date()

--- PRECourse/TypeScript/hello.js
+++ PRECourse/TypeScript/hello.js
@@ -1,5 +1,5 @@
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
-    console.log("Hello " + person + ", today is " + date + "!");
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
 }
-greet("Brendan");
+greet("Roger", new Date());
