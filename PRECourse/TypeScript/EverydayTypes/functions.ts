231ac2b81e53b33ed33d0183dfde1e674885f630
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-04-25 21:36:29 +0200

    Przekazywanie typów w funkcji

--- PRECourse/TypeScript/EverydayTypes/functions.ts
+++ PRECourse/TypeScript/EverydayTypes/functions.ts
@@ -10,3 +10,18 @@ function greet(name: string) {
 function getFavoriteNumber(): number {
     return 26;
 }
+
+// No type annotations here, but TypeScript can spot the bug
+const names = ['Alice', 'Bob', 'Eve'];
+
+// Contextual typing for function
+names.forEach(function (s) {
+    // console.log(s.toUppercase());
+    console.log(s.toUpperCase());
+});
+
+// Contextual typing also applies to arrow functions
+names.forEach((s) => {
+    // console.log(s.toUppercase());
+    console.log(s.toUpperCase());
+});
