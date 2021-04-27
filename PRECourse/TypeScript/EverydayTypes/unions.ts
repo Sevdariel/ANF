f445ed7c5a34c2ea645f38e5cab2b0c009f669c5
Sev <michal.zalewski1995@gmail.com>
2021-04-27 21:46:49 +0200

    Rozdzielenie string i number

--- PRECourse/TypeScript/EverydayTypes/unions.ts
+++ PRECourse/TypeScript/EverydayTypes/unions.ts
@@ -9,6 +9,15 @@ printId("202");
 // Error
 // printId({ myID: 22342});
 
-function printId1(id: number | string) {
-    console.log(id.toUpperCase());
+// function printId1(id: number | string) {
+//     console.log(id.toUpperCase());
+// }
+
+function narrowPrintId(id: number | string) {
+    if (typeof id === 'string') {
+        // In this branch, id is of type 'string'
+    } else {
+        // Here, id is of type 'number'
+        console.log(id);
+    }
 }
