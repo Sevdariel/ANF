5f47899e04b4e8daa3911f7e0b320b0f53d09cb2
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-05-02 16:26:08 +0200

    Sprawdzanie prawdziwości zawężania typów

--- PRECourse/TypeScript/Narrowing/typeof-type-guards.ts
+++ PRECourse/TypeScript/Narrowing/typeof-type-guards.ts
@@ -1,11 +1,9 @@
 function printAll(strs: string | string[] | null) {
-    if (typeof strs === "object") {
+    if (strs && typeof strs === "object") {
         for (const s of strs) {
             console.log(s);
         }
     } else if (typeof strs === "string") {
         console.log(strs);
-    } else {
-        // do nothing
     }
 }
