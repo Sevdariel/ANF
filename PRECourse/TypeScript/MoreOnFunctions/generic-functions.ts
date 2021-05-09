b0bb4d4e8fba647f9828e9473c78a4e9e4bdb16b
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-05-09 18:20:21 +0200

    Wnioskowanie
    Typy generyczne są wnioskowane na podstawie tego co przychodzi i tego co
    wychodzi z funkcji generycznej

--- PRECourse/TypeScript/MoreOnFunctions/generic-functions.ts
+++ PRECourse/TypeScript/MoreOnFunctions/generic-functions.ts
@@ -6,3 +6,12 @@ function firstElement<Type>(arr: Type[]): Type {
 const s = firstElement(["a", "b", "c"]);
 // n is of type 'number'
 const n = firstElement([1, 2, 3]);
+
+// Inference
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+    return arr.map(func);
+}
+
+// Parameter 'n' is of type 'string'
+// 'parsed' is of type 'number[]'
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
