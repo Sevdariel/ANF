909094fbf54e4fcd4d7f110bf9f26696c6670b80
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-05-09 19:19:14 +0200

    Optional parameters in callbacks
    Nie powinno się używać opcjonalnych parametrów w definicji funkcji
    zwrotnej ponieważ dodatkowe argumenty są ignorowane

--- PRECourse/TypeScript/MoreOnFunctions/optiona-parameters.ts
+++ PRECourse/TypeScript/MoreOnFunctions/optiona-parameters.ts
@@ -10,3 +10,25 @@ f(undefined); // ok
 function fDefault(x = 10) {
     // ...
 }
+
+// optional parameters in callbacks
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i);
+    }
+}
+
+function myForEach1(arr: any[], callback: (arg: any, index?: number) => void) {
+    for (let i = 0; i < arr.length; i++) {
+        // I don't feel like providing the index today
+        callback(arr[i]);
+    }
+}
+
+myForEach1([1, 2, 3], (a, i) => {
+    // Object is possibly 'undefined'.
+    console.log(i.toFixed());
+})
+
+myForEach([1, 2, 3], (a) => console.log(a));
+myForEach([1, 2, 3], (a, i) => console.log(a, i));
