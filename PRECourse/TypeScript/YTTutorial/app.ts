888fc2fa315d12722233553ebf8e481eab0ebdcb
Michał Zalewski <michal.zalewski@forcom.com.pl>
2021-06-02 16:32:04 +0200

    YTTutorial

---
+++ PRECourse/TypeScript/YTTutorial/app.ts
@@ -0,0 +1,44 @@
+// takie użyciie jest zbędne
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'Maximilian',
+//   age: 30
+// };
+
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  //role: [2, 'author']
+}
+const personTuple: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+// favoriteActivities = 'Sports';
+// favoriteActivities = ['Sports', 1];
+let favoriteActivitiesAny: any[];
+favoriteActivities = ['Sports'];
+
+console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  // console.log(hobby.map());
+}
+
