5c79423ce37a04652b98c8ef60ff20daeced5e44
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-05 18:23:05 +0200

    Mapowanie modyfikatorów
    Można usuwać modyfikatory readonly i ?.

---
+++ PRECourse/TypeScript/Handbook/TypeManipulation/Mapped Types/mapping-modifiers.ts
@@ -0,0 +1,24 @@
+// Removes 'readonly' attributes from a type's properties
+type CreateMutable<Type> = {
+    -readonly [Property in keyof Type]: Type[Property];
+};
+
+type LockedAccount = {
+    readonly id: string;
+    readonly name: string;
+};
+
+type UnlockedAccount = CreateMutable<LockedAccount>;
+
+// Removes 'optiona' attributes from a type's properties
+type Concrete<Type> = {
+    [Property in keyof Type]-?: Type[Property];
+};
+
+type MaybeUser = {
+    id: string;
+    name?: string;
+    age?: number;
+};
+
+type User = Concrete<MaybeUser>;
