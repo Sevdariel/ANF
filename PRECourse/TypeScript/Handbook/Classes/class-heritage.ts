485ed6d6a1210cfd67776c8b1e7ac0717b5b8f02
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-06 12:02:34 +0200

    Rozszerzanie klas

--- PRECourse/TypeScript/Handbook/Classes/class-heritage.ts
+++ PRECourse/TypeScript/Handbook/Classes/class-heritage.ts
@@ -40,3 +40,23 @@ class C1 implements A {
 const c = new C();
 // c.y = 10;
 // Property 'y' does not exist on type 'C'.
+
+class Animal {
+    move() {
+        console.log("Moving along!");
+    }
+}
+
+class Dog extends Animal {
+    woof(times: number) {
+        for (let i = 0; i < times; i++) {
+            console.log("woof!");
+        }
+    }
+}
+
+const d = new Dog();
+// Base class method
+d.move();
+// Derived class method
+d.woof(3);
