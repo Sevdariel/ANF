15184228b964b1c064c71865493e7cf68c36541c
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-06 11:29:58 +0200

    Uwagi

--- PRECourse/TypeScript/Handbook/Classes/class-heritage.ts
+++ PRECourse/TypeScript/Handbook/Classes/class-heritage.ts
@@ -15,3 +15,28 @@ class Sonar implements Pingable {
 //         console.log("pong!");
 //     }
 // }
+
+interface Checkable {
+    check(name: string): boolean;
+}
+
+class NameChecker implements Checkable {
+    check(s) {
+        // Parameter 's' implicitly has an 'any' type.
+        // Notice no error here
+        return s.toLowerCase() === "ok";
+    }
+}
+
+interface A {
+    x: number;
+    y?: number;
+}
+
+class C1 implements A {
+    x = 0;
+}
+
+const c = new C();
+// c.y = 10;
+// Property 'y' does not exist on type 'C'.
