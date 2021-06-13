47c49480fba1ec11f3c59b33ec4752526b10ad10
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-13 16:55:28 +0200

    Specyficzne zachowanie js

---
+++ PRECourse/TypeScript/Handbook/Classes/runtime-in-classes.ts
@@ -0,0 +1,15 @@
+class MyClass {
+    name = "MyClass";
+    getName() {
+        return this.name;
+    }
+}
+
+const c = new MyClass();
+const obj = {
+    name: "obj",
+    getName: c.getName,
+};
+
+//Prints "obj", not "MyClass"
+console.log(obj.getName());
