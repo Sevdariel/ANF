60aba13a28412560894ff3ba34b7ac0dde5304ac
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-13 18:20:51 +0200

    Abstract construct signature

--- PRECourse/TypeScript/Handbook/Classes/abstract-classes-members.ts
+++ PRECourse/TypeScript/Handbook/Classes/abstract-classes-members.ts
@@ -22,3 +22,13 @@ d.printName();
     // Non-abstract class 'Derived' does not implement inherited abstract member 'getName' from class 'Base'.
     //forgot to do anything
 // }
+
+function greet(ctor: new () => Base) {
+    const instance = new ctor();
+    instance.printName();
+}
+
+greet(Derived);
+// greet(Base);
+// Argument of type 'typeof Base' is not assignable to parameter of type 'new () => Base'.
+//   Cannot assign an abstract constructor type to a non-abstract constructor type.
