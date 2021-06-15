b5705562645cde3dbaee9cfbb8aca97593f783d7
Michał Zalewski <michal.zalewski@forcom.com.pl>
2021-06-15 13:49:41 +0200

    Start of literal types

--- PRECourse/TypeScript/YTTutorial/app.js
+++ PRECourse/TypeScript/YTTutorial/app.js
@@ -1,49 +1,14 @@
-// takie użyciie jest zbędne
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: 'Maximilian',
-//   age: 30
-// };
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-};
-var personTuple = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-};
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var personEnum = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin');
-// person.role[1] = 10;
-// person.role = [0, 'admin', 'user'];
-var favoriteActivities;
-// favoriteActivities = 'Sports';
-// favoriteActivities = ['Sports', 1];
-var favoriteActivitiesAny;
-favoriteActivities = ['Sports'];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
-    // console.log(hobby.map());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (personEnum.role === Role.AUTHOR) {
-    console.log('is author');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
--- PRECourse/TypeScript/YTTutorial/app.ts
+++ PRECourse/TypeScript/YTTutorial/app.ts
@@ -1,58 +1,16 @@
-// takie użyciie jest zbędne
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: 'Maximilian',
-//   age: 30
-// };
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
 
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  //role: [2, 'author']
-}
-const personTuple: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
-};
-
-enum Role {
-  ADMIN, READ_ONLY, AUTHOR
-};
-
-const personEnum = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
-
-// person.role.push('admin');
-// person.role[1] = 10;
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[];
-// favoriteActivities = 'Sports';
-// favoriteActivities = ['Sports', 1];
-let favoriteActivitiesAny: any[];
-favoriteActivities = ['Sports'];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby);
-  // console.log(hobby.map());
+  return result;
 }
 
-if (personEnum.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
