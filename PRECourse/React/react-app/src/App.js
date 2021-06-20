0a291c04f2bc16cf5aa66edc5d86454d8a6a8731
Micha³ Zalewski <michal.zalewski1995@gmail.com>
2021-06-20 12:24:34 +0200

    Display

--- PRECourse/React/react-app/src/App.js
+++ PRECourse/React/react-app/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    );
   }
 }
 
